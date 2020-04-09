c5f536d0b3acf099f47cf216b9e327f9e5362094
Derric Fragosa <fderric@gmail.com>
2020-04-09 17:43:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -75,7 +75,8 @@ class Dog
     SELECT * FROM dogs WHERE name = ?
     LIMIT 1
     SQL
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
   end
       
 end
