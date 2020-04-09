50b32b9e2638c248170e2a69fa71f004b95cf931
Derric Fragosa <fderric@gmail.com>
2020-04-09 16:14:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,7 +65,7 @@ class Dog
     if dog
       new_dog = self.new_from_db(dog)
     else 
-      dog = self.create(name: name, breed: breed)
+      new_dog = self.create(name: name, breed: breed)
     end
     dog
   end
