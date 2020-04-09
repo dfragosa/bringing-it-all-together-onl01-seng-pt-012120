58001ff48f2b8583d48f4ca7674425adb51bab27
Derric Fragosa <fderric@gmail.com>
2020-04-09 16:12:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,7 +63,7 @@ class Dog
       SQL
    dog = DB[:conn].execute(sql, name, breed).first
     if dog
-      new_dog = Dog.new(dog)
+      new_dog = self.new(dog)
     else 
       dog = self.create(name: name, breed: breed)
     end
